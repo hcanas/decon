522d11bb84f2e82aec0375b8536b730ac0dcdc3a
hcanas <harveyanas@gmail.com>
2024-02-12 13:34:18 +0800

    shrink size

--- resources/js/Components/DarkModeToggle.vue
+++ resources/js/Components/DarkModeToggle.vue
@@ -21,21 +21,21 @@ watch(() => enabled.value, () => {
     <SwitchGroup>
         <div class="flex items-baseline space-x-1">
             <SwitchLabel>
-                <FontAwesomeIcon :icon="faSun" class="w-5 h-5 text-gray-300" />
+                <FontAwesomeIcon :icon="faSun" class="w-4 h-4 text-gray-300" />
             </SwitchLabel>
             <Switch
                 v-model="enabled"
                 :class="enabled ? 'bg-gray-900' : 'bg-gray-300'"
-                class="relative inline-flex h-5 w-14 shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75"
+                class="relative inline-flex h-4 w-10 shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75"
             >
                 <span
                     aria-hidden="true"
-                    :class="enabled ? 'translate-x-9' : 'translate-x-0'"
-                    class="pointer-events-none inline-block h-4 w-4 transform rounded-full bg-white shadow-lg ring-0 transition duration-200 ease-in-out"
+                    :class="enabled ? 'translate-x-6' : 'translate-x-0'"
+                    class="pointer-events-none inline-block h-3 w-3 transform rounded-full bg-white shadow-lg ring-0 transition duration-200 ease-in-out"
                 />
             </Switch>
             <SwitchLabel>
-                <FontAwesomeIcon :icon="faMoon" class="w-5 h-5 text-gray-900" />
+                <FontAwesomeIcon :icon="faMoon" class="w-4 h-4 text-gray-800" />
             </SwitchLabel>
         </div>
     </SwitchGroup>
